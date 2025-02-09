810b406eb31fe7cf7d8f3420498816e855ad5cb9
Aditya W <aditya@cube.asia>
2025-02-09 16:04:23 +0700

    fix: rollback to tailwindv3 since it cannot parse templ right now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./internal/components/**/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 };
